27a9b9ae0796c1412e6c4305f0640c2f7ee1abf3
Bruno Miličević <bruno.milicevic@gmail.com>
2021-08-24 12:20:36 +0200

    Add a failing test to see if workflow will fail

--- src/test/java/com/example/heroku/HerokuApplicationTests.java
+++ src/test/java/com/example/heroku/HerokuApplicationTests.java
@@ -2,15 +2,24 @@ package com.example.heroku;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
+
 public class HerokuApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void shouldFailSometimes() {
+		Random r = new Random();
+		assertEquals(0, r.nextInt(99) % 2);
 	}
 
 }
